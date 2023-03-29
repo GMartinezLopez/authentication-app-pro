8a05b0beb1088aeed886da730d2573ca8bb48193
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-28 21:48:25 -0700

    feat: add post endpoints

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const authController = require('../controllers/authController');
-const bodyParser = require('body-parser');
-const router = express.Router();
-const User = require('../models/user');
+const express = require('express')
+const authController = require('../controllers/authController')
+const bodyParser = require('body-parser')
+const router = express.Router()
+const User = require('../models/user')
+const blogController = require('../controllers/blogController')
 
 // Parse request body
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({ extended: true }))
 
 router.post('/register', async (req, res) => {
   try {
-    const { name, email, password, date } = req.body;
-    const user = new User({ name, email, password, date });
-    await user.save();
-    res.redirect('/auth/login');
+    const { name, email, password, date } = req.body
+    const user = new User({ name, email, password, date })
+    await user.save()
+    res.redirect('/auth/login')
   } catch (error) {
-    console.log(error);
-    res.render('register', { message: 'Error creating user' });
+    console.log(error)
+    res.render('register', { message: 'Error creating user' })
   }
-});
+})
 
-router.post('/login', authController.login);
-router.get('/register', authController.getRegister);
-router.get('/login', authController.getLogin);
-router.get('/home', authController.getHome);
+router.post('/login', authController.login)
+router.get('/register', authController.getRegister)
+router.get('/login', authController.getLogin)
+router.get('/home', authController.getHome)
+router.get('/blog', authController.getBlogs)
+router.post('/blog', authController.postBlog)
+router.put('/blog', authController.putBlog)
+router.delete('/blog/:id', authController.deleteBlog)
 
-module.exports = router;
+module.exports = router
