4c68eaf54d77773640b8e087a3d71df1af3759cc
DiegoAsanchezmartin <92271073+DiegoAsanchezmartin@users.noreply.github.com>
2023-03-30 12:44:50 -0500

    hola soy gay
    y me gusta airan campos vazques gta 5

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,8 +3,7 @@ const authController = require('../controllers/authController')
 const bodyParser = require('body-parser')
 const router = express.Router()
 const User = require('../models/user')
-const blogController = require('../controllers/blogController')
-
+const Publicacionesalv = require('../models/publicacionesalv')
 // Parse request body
 router.use(bodyParser.urlencoded({ extended: true }))
 
@@ -20,13 +19,26 @@ router.post('/register', async (req, res) => {
   }
 })
 
+router.post('/editPost', async (req, res) => {
+  try {
+    const { title, desc, img } = req.body
+    const publicacionesalv = new Publicacionesalv({ title, desc,img })
+    await publicacionesalv.save()
+    res.redirect('/auth/editPost')
+  } catch (error) {
+    console.log(error)
+    res.render('editPost', { message: 'Error creating post' })
+  }
+})
+
 router.post('/login', authController.login)
 router.get('/register', authController.getRegister)
 router.get('/login', authController.getLogin)
 router.get('/home', authController.getHome)
 router.get('/blog', authController.getBlogs)
-router.post('/blog', authController.postBlog)
+router.get('/publi', authController.getPubli)
 router.put('/blog', authController.putBlog)
 router.delete('/blog/:id', authController.deleteBlog)
+router.get('/editPost', authController.getEditPost)
 
 module.exports = router
